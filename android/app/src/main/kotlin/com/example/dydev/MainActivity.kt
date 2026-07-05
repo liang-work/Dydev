f5e38e27f267f115872f34aa31fedf60d47ee180
liang-work <tianhesl@outlook.com>
2026-07-05 21:21:18 +0800

    :green_heart: Fix the build issues

--- android/app/src/main/kotlin/com/example/dydev/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dydev
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/work/dynet/developer/MainActivity.kt
@@ -0,0 +1,5 @@
+package work.dynet.developer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
